991cef7a7d2f562a8d30fa6d05b172156b269e62
Дмитрий Груздев <nubile4446@mail.ru>
2025-04-08 11:50:03 +0300

    add modul reports and add func

---
+++ config.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+CURRENT_DIR = Path(__file__).parent
+TRANSACTION_PATH_EXCEL = CURRENT_DIR / "data" / "operations.xlsx"
---
+++ src/reports.py
@@ -0,0 +1,44 @@
+import logging
+from datetime import datetime
+from typing import Optional
+import pandas as pd
+
+from config import TRANSACTION_PATH_EXCEL
+
+logger = logging.getLogger("reports.py")
+file_handler = logging.FileHandler('../logs/reports.log', 'w', encoding="utf8")
+file_formater = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formater)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
+
+
+def read_excel(filename: str, datetime_to_timestamp: bool = True) -> pd.DataFrame:
+    operations_df = pd.read_excel(filename)
+    if datetime_to_timestamp:
+        operations_df["Дата операции"] = pd.to_datetime(operations_df["Дата операции"], dayfirst=True)
+    return operations_df
+
+
+def spending_by_category(transactions: pd.DataFrame, category: str, date: Optional[str] = None) -> pd.DataFrame:
+    """Функция возвращает траты по заданной категории за последние три месяца (от переданной даты)."""
+    if date is None:
+        date = datetime.now()
+    else:
+        date = pd.to_datetime(date)
+    start_date = date - pd.DateOffset(months=3)
+    transactions["Дата операции"] = pd.to_datetime(
+        transactions["Дата операции"],
+    )
+    filtered_transactions = transactions[
+        (transactions["Категория"] == category)
+        & (transactions["Дата операции"] >= start_date)
+        & (transactions["Дата операции"] <= date)]
+    sum_category = filtered_transactions["Сумма платежа"].sum()
+    logger.info(f"Найдено {len(filtered_transactions)} транзакций по категории '{category}' за последние три месяца.\n"
+                f"Потрачено: {sum_category}")
+    return sum_category
+
+
+transactions = read_excel(TRANSACTION_PATH_EXCEL)
+print(spending_by_category(transactions, "Госуслуги", "26.12.2021"))
